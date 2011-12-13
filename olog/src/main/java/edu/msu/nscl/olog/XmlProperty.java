2daf73610c4b7cd814fee3cc79c556919899d50e
Robert Gaul <gaul@frib.msu.edu>
2011-12-13 10:05:59 -0500

    New XmlProperty class to handle new XML format for properties

--- olog/src/main/java/edu/msu/nscl/olog/XmlProperty.java
+++ olog/src/main/java/edu/msu/nscl/olog/XmlProperty.java
@@ -4,7 +4,12 @@
  * All rights reserved. Use is subject to license terms and conditions.
  */
 
-package edu.msu.nscl.olog;
+package edu.msu.nscl.olog.api;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
@@ -13,103 +18,111 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * Property object that can be represented as XML/JSON in payload data.
- *
- * @author Eric Berryman taken from Ralph Lange <Ralph.Lange@helmholtz-berlin.de>
+ * 
+ * @author Eric Berryman taken from Ralph Lange
+ *         <Ralph.Lange@helmholtz-berlin.de>
  */
-@XmlType(propOrder = {"name","value","xmlLogs"})
+@XmlType(propOrder = { "name", "attributes", "xmlLogs" })
 @XmlRootElement(name = "property")
 public class XmlProperty {
-    private String name = null;
-    private String value = null;
-    private XmlLogs logs = null;
+	private String name = null;
+	private Map<String, String> attributes;
+	private XmlLogs logs = null;
 
-    /**
-     * Creates a new instance of XmlProperty.
-     *
-     */
-    public XmlProperty() {
-    }
+	/**
+	 * Creates a new instance of XmlProperty.
+	 * 
+	 */
+	public XmlProperty() {
+	}
 
-    /**
-     * Creates a new instance of XmlProperty.
-     *
-     * @param name
-     * @param value
-     */
-    public XmlProperty(String name, String value) {
-        this.value = value;
-        this.name = name;
-    }
+	/**
+	 * Creates a new instance of XmlProperty.
+	 * 
+	 * @param name
+	 * @param value
+	 */
+	public XmlProperty(String name) {
+		this.name = name;
+	}
 
-    /**
-     * Getter for property name.
-     *
-     * @return property name
-     */
-    @XmlAttribute
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @param name
+	 * @param attributes
+	 */
+	public XmlProperty(String name, Map<String, String> attributes) {
+		this.name = name;
+		this.attributes = attributes;
+	}
 
-    /**
-     * Setter for property name.
-     *
-     * @param name property name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * Getter for property name.
+	 * 
+	 * @return property name
+	 */
+	@XmlAttribute
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Getter for property value.
-     *
-     * @return property value
-     */
-    @XmlAttribute
-    public String getValue() {
-        return value;
-    }
+	/**
+	 * Setter for property name.
+	 * 
+	 * @param name
+	 *            property name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * Setter for property value.
-     *
-     * @param value property value
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
+	/**
+	 * @return the attributes
+	 */
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
 
-    /**
-     * Getter for property's XmlLogs.
-     *
-     * @return XmlChannels object
-     */
-    @XmlElement(name = "logs")
-    public XmlLogs getXmlLogs() {
-        return logs;
-    }
+	/**
+	 * @param attributes
+	 *            the attributes to set
+	 */
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
 
-    /**
-     * Setter for property's XmlLogs.
-     *
-     * @param logs XmlLogs object
-     */
-    public void setXmlLogs(XmlLogs logs) {
-        this.logs = logs;
-    }
+	/**
+	 * Getter for property's XmlLogs.
+	 * 
+	 * @return XmlChannels object
+	 */
+	@XmlElement(name = "logs")
+	public XmlLogs getXmlLogs() {
+		return logs;
+	}
 
-    /**
-     * Creates a compact string representation for the log.
-     *
-     * @param data the XmlProperty to log
-     * @return string representation for log
-     */
-    public static String toLog(XmlProperty data) {
-         if (data.logs == null) {
-            return data.getName() + "(" + data.getValue() + ")";
-        } else {
-            return data.getName() + "(" + data.getValue() + ")"
-                    + XmlLogs.toLog(data.logs);
-        }
-    }
+	/**
+	 * Setter for property's XmlLogs.
+	 * 
+	 * @param logs
+	 *            XmlLogs object
+	 */
+	public void setXmlLogs(XmlLogs logs) {
+		this.logs = logs;
+	}
+
+	/**
+	 * Creates a compact string representation for the log.
+	 * 
+	 * @param data
+	 *            the XmlProperty to log
+	 * @return string representation for log
+	 */
+	public static String toLog(XmlProperty data) {
+		if (data.logs == null) {
+			return data.getName() + "(" + data.getAttributes().toString() + ")";
+		} else {
+			return data.getName() + "(" + data.getAttributes().toString() + ")"
+					+ XmlLogs.toLog(data.logs);
+		}
+	}
 }
