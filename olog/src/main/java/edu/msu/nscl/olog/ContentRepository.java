ba049acace98ef301b2638679ffeb61cdfa3792a
Eric Berryman <berryman@frib.msu.edu>
2012-06-28 10:15:59 -0400

    changed repository default home and repository.xml location

--- olog/src/main/java/edu/msu/nscl/olog/ContentRepository.java
+++ olog/src/main/java/edu/msu/nscl/olog/ContentRepository.java
@@ -5,6 +5,7 @@
 
 package edu.msu.nscl.olog;
 
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jcr.Repository;
@@ -19,16 +20,19 @@ import org.apache.jackrabbit.j2ee.RepositoryAccessServlet;
 
 public class ContentRepository extends OlogContextListener {
     private static Repository repository;
-    
     private static Session session;
+    private static final String WEBINF = "WEB-INF";
 
     /**
      * Create an instance of ContentRepository
      */
     public ContentRepository() {
         try {
-            String xml = "repository/repository.xml";
-            String dir = "repository";
+            URL url = ContentRepository.class.getResource("ContentRepository.class");
+            String className = url.getFile();
+            String filePath = className.substring(0,className.indexOf(WEBINF) + WEBINF.length());
+            String xml = filePath+"/repository.xml";
+            String dir = "jackrabbit";
             RepositoryConfig config = RepositoryConfig.create(xml, dir);
             repository = RepositoryImpl.create(config);
             
