6f77a6186a7272aac6372f11d3db1054d791c2fe
Kunal Shroff <kunalshroff9@gmail.com>
2022-03-24 10:39:11 -0400

    remove hard coded database name

--- src/main/resources/db/mysql56/migration/V2.15__Add_Bitemporal.sql
+++ src/main/resources/db/mysql56/migration/V2.15__Add_Bitemporal.sql
@@ -1,6 +1,6 @@
 ﻿
-DROP TABLE IF EXISTS `olog`.`bitemporal_log`;
-CREATE TABLE  `olog`.`bitemporal_log` (
+DROP TABLE IF EXISTS `bitemporal_log`;
+CREATE TABLE  `bitemporal_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `log_id` int(11) unsigned DEFAULT NULL,
   `recordinterval` datetime DEFAULT NULL,
@@ -15,9 +15,9 @@ CREATE TABLE  `olog`.`bitemporal_log` (
   CONSTRAINT `logs_FK1` FOREIGN KEY (`log_id`) REFERENCES `logs` (`id`)
 );
 
-ALTER TABLE `olog`.`entries` ADD COLUMN `state` ENUM('Active','Inactive') NOT NULL DEFAULT 'Active' AFTER `created`;
+ALTER TABLE `entries` ADD COLUMN `state` ENUM('Active','Inactive') NOT NULL DEFAULT 'Active' AFTER `created`;
 
-INSERT INTO `olog`.`bitemporal_log` (
+INSERT INTO `bitemporal_log` (
 	`log_id`, 
 	`recordinterval_0`, 
 	`recordinterval`, 
@@ -26,21 +26,21 @@ INSERT INTO `olog`.`bitemporal_log` (
 	`entry_id`)
   SELECT `id`, 
          `modified`, 
-	 if((SELECT `modified` FROM `olog`.`logs` e WHERE e.`id` > l.`id` and e.`entry_id`=l.`entry_id` ORDER BY `id` LIMIT 1) is null, 
+	 if((SELECT `modified` FROM `logs` e WHERE e.`id` > l.`id` and e.`entry_id`=l.`entry_id` ORDER BY `id` LIMIT 1) is null, 
 		'3197-09-13 18:00:00', 
-		(SELECT `modified` FROM `olog`.`logs` e WHERE e.`id` > l.`id` and e.`entry_id`=l.`entry_id` ORDER BY `id` LIMIT 1)), 
+		(SELECT `modified` FROM `logs` e WHERE e.`id` > l.`id` and e.`entry_id`=l.`entry_id` ORDER BY `id` LIMIT 1)), 
 	`modified`, 
 	'3197-09-13 18:00:00',
 	`entry_id` 
-	from `olog`.`logs` l;
+	from `logs` l;
 
-ALTER TABLE `olog`.`bitemporal_log` ADD INDEX `record_0` USING BTREE(`recordinterval_0`),
+ALTER TABLE `bitemporal_log` ADD INDEX `record_0` USING BTREE(`recordinterval_0`),
  ADD INDEX `record` USING BTREE(`recordinterval`),
  ADD INDEX `valid_0` USING BTREE(`validityinterval_0`),
  ADD INDEX `valid` USING BTREE(`validityinterval`);
 
-ALTER TABLE `olog`.`bitemporal_log` ADD INDEX `ids`(`log_id`, `entry_id`);
+ALTER TABLE `bitemporal_log` ADD INDEX `ids`(`log_id`, `entry_id`);
 
-UPDATE `olog`.`bitemporal_log` SET recordinterval='3197-09-13 18:00:00' where recordinterval_0 > recordinterval;
+UPDATE `bitemporal_log` SET recordinterval='3197-09-13 18:00:00' where recordinterval_0 > recordinterval;
 
-ALTER TABLE `olog`.`logs_attributes` MODIFY COLUMN `grouping_num` INT(11) DEFAULT NULL;
+ALTER TABLE `logs_attributes` MODIFY COLUMN `grouping_num` INT(11) DEFAULT NULL;
